23221b38bb09ace3b95ac14b6de5294057861a9f
Fatima Musa <mpatigul@my.centennialcollege.ca>
2022-10-07 09:22:35 -0400

    server.js restructured for throubleshooting

--- server.js
+++ server.js
@@ -1,38 +1,58 @@
-// import third-party module 
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import session from 'express-session';
+import debug from 'debug';
+debug('comp-229');
+import http from 'http';
 
-// ES Modules fix for __dirname
-import path, {dirname} from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import app from './app/app.js';
 
-//Import Router
+const PORT = normalizePort(process.env.PORT || 3000);
+app.set('port', PORT);
 
-import indexRouter from './app/routes/index.route.server.js';
+const server = http.createServer(app);
 
-// instantiate app-server
-const app = express();
+server.listen(PORT);
+server.on('error', onError);
+server.on('listening', onListening);
 
-// setup viewEngine EJS
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
 
-app.set('views', path.join(__dirname, '/app/views'));
-app.set('view engine ','ejs');
+    if (port >= 0) {
+        return port;
+    }
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(session({secret: 'MySecret', saveUninitialized: false, resave: false}));
+    return false;
+}
 
-// use router
-app.use('/', indexRouter);
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
 
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
 
-// run app
-app.listen(3000);
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
 
-console.log('Server running at http://localhost:3000');
+function onListening() 
+{
+  let addr = server.address();
+  let bind = 'pipe ' + addr;
+  debug('Listening on ' + bind);
+}
